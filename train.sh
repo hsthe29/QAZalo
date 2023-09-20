970144820a6eeca3d9c67c3adccc43a3719c55d2
hsthe29 <The.HS200614@sis.hust.edu.vn>
2023-09-21 00:59:58 +0700

    update sh files

---
+++ train.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+C:/Users/TheHS/anaconda3/envs/dl/python.exe main.py --mode=train --EPOCHS=5 --batch-size=16 --from-logits=True --use-pooler=False --train-input=data/train/train.csv --validation-input=data/train/val.csv
